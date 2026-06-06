e9c8298fb788c66366627bfb6b10184a9d40ca86
rayanxweb-private <aryudhaamanaronian@gmail.com>
2026-06-06 09:50:31 +0700

    Update firebase-admin.ts

--- src/config/firebase-admin.ts
+++ src/config/firebase-admin.ts
@@ -1,3 +1,4 @@
+// src/config/firebase-admin.ts
 import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
@@ -8,16 +9,13 @@ if (!admin.apps.length) {
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       }),
-      databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`,
       storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     });
   } catch (error) {
-    console.error('Firebase Admin Initialization Error:', error);
+    console.error('Firebase admin initialization error during build pipeline:', error);
   }
 }
 
 export const adminAuth = admin.auth();
-export const adminDb = admin.firestore();
-export const adminRtdb = admin.database();
 export const adminMessaging = admin.messaging();
 export const adminStorage = admin.storage();
